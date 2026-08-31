01cb9e73fd5f1e2b1da626d9bc3c5b9c87a1d101
anhnam26 <222565804+anhnam26@users.noreply.github.com>
2026-09-01 02:23:09 +0700

    Add missing findNavController import in LibraryFragment.kt

--- app/src/main/java/com/google/mediapipe/examples/poselandmarker/fragment/LibraryFragment.kt
+++ app/src/main/java/com/google/mediapipe/examples/poselandmarker/fragment/LibraryFragment.kt
@@ -16,6 +16,7 @@ import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.textfield.TextInputEditText
